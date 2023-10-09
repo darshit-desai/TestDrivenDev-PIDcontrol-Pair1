54c0140f8af642b07f65cdb1ba9df07a6ed5dcb7
DARSHIT DESAI <desai0681@gmail.com>
2023-10-09 18:09:47 -0400

    Update PIDController.cpp
    Updating the stubs with the method implementations

--- app/PIDController.cpp
+++ app/PIDController.cpp
@@ -1,6 +1,7 @@
 /**
  * @file PIDController.cpp
  * @author (Part 1) Sai Teja Gilukara (saitejag@umd.edu) - Driver, Aaqib Barodawala (aakib@umd.edu) - Navigator
+ * @author (Part 2) Darshit Desai (darshit@umd.edu) - Driver, Sameer Arjun S (ssarjun@umd.edu) - Navigator
  * @brief CPP file for PIDController class
  * @version 0.1
  * @date 2023-10-06
@@ -10,7 +11,8 @@
  */
 
 #include "../include/PIDController.hpp"
-
+#include <stdlib.h>
+#include <iostream>
 /**
  * @brief Construct a new PIDController::PIDController object
  * 
@@ -20,6 +22,9 @@ tddgroup7_pair1::PIDController::PIDController() {
    * @todo Initialize the Kp, Ki and Kd values
    * 
    */
+  kp = 0.0;
+  ki = 0.0;
+  kd = 0.0;
 }
 
 /**
@@ -34,7 +39,13 @@ double tddgroup7_pair1::PIDController::computeError(double setPoint, double curr
    * @todo Compute the error using setPoint and currentState 
    * 
    */
-  return 0;
+  double error = setPoint - currentState;
+  double total_feedback = 0.0;
+  total_feedback = computeFeedback(error);
+  errorVector.push_back(error);
+  currentState += total_feedback;
+  error = setPoint - currentState;
+  return error;
 }
 /**
  * @brief Compute the feedback
@@ -47,7 +58,25 @@ double tddgroup7_pair1::PIDController::computeFeedback(double error) {
    * @todo Compute the feedback using error
    * 
    */
-  return 0;
+  double Proportional_error{0};
+  double Integral_error{0};
+  double Differential_error{0};
+  double total_feedback{0};
+  Proportional_error = kp*(error);
+  // double error = setPoint - currentState;
+  if(errorVector.size()>0){
+    for(int i=0;i<errorVector.size();i++){
+      Integral_error += errorVector[i];
+    }
+    Differential_error = kd*(error - errorVector[errorVector.size()-1]);
+  }
+  else{
+    Differential_error = 0.0;
+  }
+  Integral_error += error;
+  Integral_error = ki*Integral_error/(errorVector.size()+1);
+  total_feedback = Proportional_error + Integral_error + Differential_error;
+  return total_feedback;
 }
 /**
  * @brief Get the Kp object
@@ -59,7 +88,7 @@ double tddgroup7_pair1::PIDController::getKp() {
    * @todo Get the Kp value
    * 
    */
-  return 0;
+  return kp;
 }
 /**
  * @brief Get the Ki object
@@ -71,7 +100,7 @@ double tddgroup7_pair1::PIDController::getKi() {
    * @todo Get the Ki value
    * 
    */
-  return 0;
+  return ki;
 }
 /**
  * @brief Get the Kd object
@@ -83,7 +112,7 @@ double tddgroup7_pair1::PIDController::getKd() {
    * @todo Get the Kd value
    * 
    */
-  return 0;
+  return kd;
 }
 /**
  * @brief Set the Kp object
@@ -95,6 +124,7 @@ void tddgroup7_pair1::PIDController::setKp(double Kp) {
    * @todo Set the Kp value
    * 
    */
+  kp = Kp;
 }
 /**
  * @brief Set the Ki object
@@ -106,6 +136,7 @@ void tddgroup7_pair1::PIDController::setKi(double Ki) {
    * @todo Set the Ki value
    * 
    */
+  ki = Ki;
 }
 /**
  * @brief Set the Kd object
@@ -117,5 +148,5 @@ void tddgroup7_pair1::PIDController::setKd(double Kd) {
    * @todo Set the Kd value
    * 
    */
+  kd = Kd;
 }
-
