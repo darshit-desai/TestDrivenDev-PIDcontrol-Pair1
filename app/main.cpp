b4929642af106a078e31bc7c16ca3c09227b8e8b
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-10-09 17:33:29 -0400

    Added PID implementation
    Code for PID implementation with hpp and cpp files added along with their CMakeLists.txt. This is a stub and will be implemented later

---
+++ app/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Sai Teja Giluara (saitejag@umd.edu) - Driver, Aaqib Barodawala (aakib@umd.edu) - Navigator
+ * @brief A program to run the control system using PIDController class
+ * @version 0.1
+ * @date 2023-10-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "PIDController.hpp"
+int main() {
+  return 0;
+}
